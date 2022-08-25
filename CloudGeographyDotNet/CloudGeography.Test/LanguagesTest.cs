8935de6bbc9c89bfd80802745b0cb0d262157346
Mohammad Mohammad <MohammadMohammad@angrymonkeyagency.com>
2022-08-25 16:01:41 +0300

    Adding Test Methods

--- CloudGeographyDotNet/CloudGeography.Test/LanguagesTest.cs
+++ CloudGeographyDotNet/CloudGeography.Test/LanguagesTest.cs
@@ -4,13 +4,36 @@ namespace CloudGeography.Test
 	public class LanguagesTest
 	{
 		[TestMethod]
-		public void Get_Languages_By_Country()
+		public void Get_All_Languages()
 		{
 			CloudGeographyClient client = new();
-
-			List<CountryLanguage> languages = client.Languages.GetByCountry("US");
-
+			List<Language> languages = client.Languages.GetAll();
 			Assert.IsTrue(languages.Any());
+		}
+
+		[TestMethod]
+		public void Get_Languages_By_Language_Codes()
+		{
+			CloudGeographyClient client = new();
+			List<Language> languages = client.Languages.Get(new[] { "AA" , "AF" });
+			Assert.IsTrue(languages.Any());
+		}
+
+
+		[TestMethod]
+		public void Get_Country_Languages_By_Country_Code()
+		{
+			CloudGeographyClient client = new();
+			List<CountryLanguage> languages = client.Languages.GetByCountry("US");
+			Assert.IsTrue(languages.Any());
+		}
+
+		[TestMethod]
+		public void Get_Language_By_Language_Code()
+		{
+			CloudGeographyClient client = new();
+			Language ?language = client.Languages.Get("EN");
+			Assert.AreEqual("English", language?.Name);
 		}
 	}
 }
